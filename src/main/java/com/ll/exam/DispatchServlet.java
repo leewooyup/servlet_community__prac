d820a847856ebfad9c6a96a8889ec00e4dd37158
leewooyup <zjadlspun1114@naver.com>
2022-08-10 20:35:47 +0900

    세팅

---
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -0,0 +1,10 @@
+package com.ll.exam;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@WebServlet("/usr/*")
+public class DispatchServlet extends HttpServlet {
+}
